8bf6942850995b5d639dfcf55e5aa09c99d5f60f
Ferdous ahmed <helloferdous@gmail.com>
2020-07-13 17:32:30 +0600

    first commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    date=models.DateField()
+
+def __str__(self):
+    return self.title
+
---
+++ portfolio/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Project(models.Model):
+    title=models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    urls=models.URLField(blank=True)
+    image= models.ImageField( upload_to='portfolio/images')
+
+def __str__(self):
+    return self.title
