5777a0b478e54d6e2db3a00beedb5389151f250f
ferdous <helloferdous@gmail.com>
2020-07-19 10:14:38 +0000

    removed old stuff

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_w)2(u6g*h$f%+bsbl++%y*+)3f(e(b$*=e#ci2y%e@*dcr^rx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ferdous94.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
