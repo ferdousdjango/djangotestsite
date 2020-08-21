ae4d9a3760b7ecab0c2a290e1efb5dbdfb7fdc4a
Ferdous ahmed <helloferdous@gmail.com>
2020-08-21 21:14:15 +0600

    fixed an issue with logo

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Project(models.Model):
     title=models.CharField(max_length=100)
     description = models.TextField(max_length=300)
-    urls=models.URLField(blank=True)
     image= models.ImageField( upload_to='portfolio/images')
+    urls=models.URLField(blank=True)
 
     def __str__(self):
         return self.title
